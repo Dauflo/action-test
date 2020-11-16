d7c3bb0d94c1a4c3725252ff9d71dfeb5dc659eb
Dauflo <284994@supinfo.com>
2020-11-16 14:45:44 +0100

    try things

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-try {
-    const nameToGreet = core.getInput("who-to-greet")
-    console.log(`Hello ${nameToGreet}`)
-    const time = (new Date()).toTimeString()
-    core.setOutput("time", time)
+async function run() {
+    try {
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        let owner = payload.repository.owner.name
+        let repo = payload.repository.name
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
-} catch (error) {
-    core.setFailed(error.message)
+        console.log(owner, repo)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
+
+run()
