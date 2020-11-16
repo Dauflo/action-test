08e09cd9324cf8b6f84883fc1972a4eee2bf5c33
Dauflo <284994@supinfo.com>
2020-11-16 15:02:54 +0100

    good

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ async function run() {
         // const payload = JSON.stringify(github.context.payload, undefined, 2)
         // console.log(payload)
 
-        const myToken = core.getInput("myToken")
+        const myToken = core.getInput("who-to-greet")
         const octokit = github.getOctokit(myToken)
         let owner = github.context.payload.repository.owner.name
         let repo = github.context.payload.repository.name
 
         console.log(owner, repo)
 
-        console.log(octokit.repos.listReleases({owner, repo}))
+        console.log(await octokit.repos.listReleases({owner, repo}))
     } catch (error) {
         core.setFailed(error.message)
     }
