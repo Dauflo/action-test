c8dd7cd2f5e9b8bd9240b3739b43bc89a6b3a11f
Dauflo <284994@supinfo.com>
2020-11-17 08:40:45 +0100

    clean

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const fs= require("fs")
 
 async function run() {
     try {
-        // const payload = JSON.stringify(github.context.payload, undefined, 2)
-        // console.log(payload)
-
         const myToken = core.getInput("who-to-greet")
         const octokit = github.getOctokit(myToken)
         let owner = github.context.payload.repository.owner.name
@@ -17,7 +14,6 @@ async function run() {
 
         let lastRelease =  await (await octokit.repos.listReleases({owner, repo})).data[0].tag_name
 
-        // console.log(await exec.exec(`git diff-tree --name-only HEAD..${lastRelease}`))
         exec(`git diff-tree --name-only HEAD..${lastRelease}`, (error, stdout, stderr) => {
             folders = stdout.split("\n")
             for (let folder of folders) {
